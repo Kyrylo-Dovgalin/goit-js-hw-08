ba2dd9420207d123045be94094e7f3f1cee5b86c
Kyrylo Dovgalin <kyrylo.dovgalin@gmail.com>
2022-11-08 12:17:04 +0200

    Task#2 Done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,24 @@
+import Player from '@vimeo/player';
+import Throttle from 'lodash.throttle';
 
+// Variables
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+// 2. create variable for function videoplayer-current-time (for easy use). according to the task condition
+const timekey = 'videoplayer-current-time';
+// created a function. by restructuring the seconds were retrieves from the object "time update" (recorded in localStorage).
+function durationSavelocalstorage({ seconds }) {
+  localStorage.setItem(timekey, seconds);
+}
+// the palayer restarted when the page was loaded
+window.addEventListener('load', newStart);
+// 1, 4. started the 'on' method and added a delay
+player.on('timeupdate', Throttle(durationSavelocalstorage, 1000));
+function newStart() {
+  if (!localStorage.getItem(timekey)) {
+    return;
+  }
+  const currentVideoTime = localStorage.getItem(timekey);
+  //3. set 0 for zero listening
+  player.setCurrentTime(currentVideoTime ?? 0);
+}
